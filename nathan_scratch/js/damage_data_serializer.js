5ac8232e35be6b94355a68824a9d37119fa6d095
UltrascatterRED <natbarton14@gmail.com>
2024-11-11 15:24:29 -0500

    javascript form submission testing

---
+++ nathan_scratch/js/damage_data_serializer.js
@@ -0,0 +1,12 @@
+function convertToJson() {
+    let form = document.getElementById("damageReport");
+    let formData = {};
+    for (let i = 0; i < form.elements.length; i++) {
+        let element = form.elements[i];
+        if (element.type !== "submit") {
+            formData[element.name] = element.value;
+        }
+    }
+    let jsonData = JSON.stringify(formData);
+    console.log(jsonData);
+}
